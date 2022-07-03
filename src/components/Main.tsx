b9f662ad9f3b78fa960c4eed574c9d9581823a9e
Agustin Kassis <webmaster@masize.com>
2022-07-03 19:54:45 -0300

    Initial commit

---
+++ src/components/Main.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+import { Web3Provider } from "@ethersproject/providers";
+
+import styled from "styled-components";
+
+interface IMainProps {
+  account: string;
+  library: Web3Provider;
+}
+
+const Main = (props: IMainProps) => {
+  return (
+    <div>
+      <h1>Hola Chicos</h1>
+      <div>
+        <div>Address: {props.account}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
