ee45fd1ae86df30feb2120ec9488d5ff1b06072b
Agustin Kassis <webmaster@masize.com>
2022-07-11 01:58:17 -0300

    added Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+const Footer = (props: any) => <div>Footer</div>;
+
+Footer.defaultProps = {
+  //   fetching: false,
+  //   outline: false,
+  //   type: "button",
+  //   color: "lightBlue",
+  //   disabled: false,
+  //   icon: null,
+  //   left: false,
+};
+
+export default Footer;
