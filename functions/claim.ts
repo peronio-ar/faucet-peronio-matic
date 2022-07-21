38cbecee59c4d525493c0e9c50a452e7d8acc00f
Agustin Kassis <webmaster@masize.com>
2022-07-21 17:04:47 -0300

    added claim functions

--- functions/claim.ts
+++ functions/claim.ts
@@ -1,9 +1,30 @@
 import { Handler } from "@netlify/functions";
+import { isClaimable, setWordClaimed } from "./lib/firestore";
 
 const handler: Handler = async (event, context) => {
+  let status = 200;
+  let message;
+
+  const address = "0xdirearecibir";
+  const word = "achilles";
+
+  if (!(await isClaimable(word))) {
+    status = 423;
+    message = { message: "No claimable" };
+  } else {
+    const tx = "0x234324234234";
+    // tx = await faucetClaim(address);
+
+    await setWordClaimed(word, address, tx);
+    message = {
+      tx,
+      address,
+    };
+  }
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    statusCode: status,
+    body: JSON.stringify(message),
   };
 };
 
