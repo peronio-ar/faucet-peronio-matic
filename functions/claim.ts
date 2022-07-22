8ff5c8f6d490dbd8ef25bde6a3523f39e860da0c
Agustin Kassis <webmaster@masize.com>
2022-07-22 01:25:16 -0300

    updated query functions

--- functions/claim.ts
+++ functions/claim.ts
@@ -1,19 +1,18 @@
 import { Handler } from "@netlify/functions";
 import { isClaimable, setWordClaimed } from "./lib/firestore";
+import { sendFunds } from "./lib/blockchain";
 
 const handler: Handler = async (event, context) => {
   let status = 200;
   let message;
 
-  const address = "0xdirearecibir";
-  const word = "achilles";
+  const { word, address } = event.queryStringParameters;
 
   if (!(await isClaimable(word))) {
     status = 423;
     message = { message: "No claimable" };
   } else {
-    const tx = "0x234324234234";
-    // tx = await faucetClaim(address);
+    const tx = await sendFunds(address);
 
     await setWordClaimed(word, address, tx);
     message = {
