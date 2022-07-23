73d60912be80bf439c6a1f62a0729c1a31a0b991
Agustin Kassis <webmaster@masize.com>
2022-07-23 12:41:30 -0300

    added error log

--- src/components/widgets/ClaimResponse.tsx
+++ src/components/widgets/ClaimResponse.tsx
@@ -2,7 +2,15 @@
 import React from "react";
 import styled from "styled-components";
 
-import { Stack, Paper } from "@mui/material";
+import {
+  Stack,
+  Paper,
+  Accordion,
+  AccordionSummary,
+  Typography,
+  AccordionDetails,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 // import styled from "styled-components";
 
@@ -27,9 +35,33 @@ const ClaimResponse = (props: any) => {
   return (
     <Container>
       <Stack spacing={2}>
-        {}
-        <div>Reclamadooo</div>
-        <div>{JSON.stringify(props)}</div>
+        {props.status === 200 ? (
+          <div>
+            <div>
+              <h3>Bienvenido compañero!</h3>
+            </div>
+            <div>{props.tx}</div>
+          </div>
+        ) : (
+          <div>
+            <div>
+              <h3>Hubo un error</h3>
+            </div>
+            <div>{props.message}</div>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls='debug'
+                id='debug'
+              >
+                <Typography>Log</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>{JSON.stringify(props)}</Typography>
+              </AccordionDetails>
+            </Accordion>
+          </div>
+        )}
       </Stack>
     </Container>
   );
